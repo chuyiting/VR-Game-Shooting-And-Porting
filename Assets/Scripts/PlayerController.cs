e2c067abb1dd3b8b68dbc72bb0b3b56320ba866c
Chu Yi-Ting <e0418218@nus.edu.sg>
2022-03-04 16:24:06 +0800

    Add teleportation script (untested)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public SteamVR_Actions_Vector2 input;
-    public float speed = 1;
-    private CharacterController characterController;
 
     private void Start()
     {
-        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // avoid confusion between teleportation
-        if (input.axis.magnitude > 0.1f)
-        {
-            // turn face to the moving direction
-            Vector3 direction = PlayerController.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-            characterController.move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction.Vector3.Up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
-        }
 
     }
 }
