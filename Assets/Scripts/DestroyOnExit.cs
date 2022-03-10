8d28de059469eb931db10b4a8e39c34aa84e186d
Luke <tankengkailuke@gmail.com>
2022-03-10 15:41:29 +0800

    dying animations

--- Assets/Scripts/DestroyOnExit.cs
+++ Assets/Scripts/DestroyOnExit.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate180 : StateMachineBehaviour
+public class DestroyOnExit : StateMachineBehaviour
 {
-    public GameObject gameObject;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.transform.Rotate(0, 180, 0);
+        Destroy(animator.gameObject, stateInfo.length);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
